6bcf66fcd2b2987f9d0ea0f765e09f8f66240bcc
twananoori <twananoori.se@gmail.com>
2022-01-27 22:35:06 +0100

    Player v1.0

--- src/main/java/BattleField.java
+++ src/main/java/BattleField.java
@@ -1,3 +1,58 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
 public class BattleField {
-    //kommentar
+    Terminal terminal;
+    private final int COLUMNS = 130;
+    private final int ROWS = 35;
+    private Player player;
+
+    public BattleField(){
+
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
+        terminalFactory.setInitialTerminalSize(new TerminalSize(COLUMNS,ROWS));
+        try {
+            terminal = terminalFactory.createTerminal();
+            terminal.setCursorVisible(false);
+        }catch (IOException e){
+            System.out.println(e);
+        }
+        player = new Player(1,terminal,ROWS);
+        handleKeyStrokes();
+
+   }
+   private void handleKeyStrokes(){
+
+       KeyStroke keyStroke = null;
+       boolean continueGame = true;
+       try {
+       while(continueGame) {
+           do {
+               Thread.sleep(5);
+               keyStroke = terminal.pollInput();
+
+           }while (keyStroke == null);
+
+           if (keyStroke.getCharacter() != null) {
+                if (keyStroke.getCharacter() == 'q' || keyStroke.getKeyType().equals(KeyType.EOF)) {
+                   continueGame = false;
+                   terminal.close();
+               }
+           }
+           switch (keyStroke.getKeyType()){
+               case ArrowUp -> player.move(-1);
+               case ArrowDown -> player.move(1);
+           }
+
+       }
+       } catch (Exception e) {
+           System.out.println(e);
+       }
+
+   }
 }
--- src/main/java/HelicopterModel.java
+++ src/main/java/HelicopterModel.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class HelicopterModel {
+    private int[][] coordinates;
+    private final char icon = '+';
+    private final int HEIGHT;
+    private final int WIDTH;
+
+    public HelicopterModel(){
+        coordinates = new int[][]{
+                                  {0, 0},{0,1},{0,2},{0,3},
+                                  {1, 0},{1,1},
+                                  {2, 0},{2,1},{2,2},{2,3}};
+        HEIGHT = 3;
+        WIDTH = 4;
+    }
+
+    public int[][] getCoordinates() {
+        return coordinates;
+    }
+    public char getIcon(){
+        return icon;
+    }
+
+    public int getHEIGHT() {
+        return HEIGHT;
+    }
+    public int getWIDTH() {
+        return WIDTH;
+    }
 }
